d59e38a00ec8522c5a5f927e5c353ac6ac69dcb7
sefeek <nikshay@ontoborn.com>
2021-09-07 22:37:31 +0530

    added explanatory comments and refactored code
    nikshay

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const axios = require("axios").default;
 const fs = require("fs");
 const sodium = require("tweetsodium");
@@ -10,13 +12,16 @@ const sodium = require("tweetsodium");
 
 **/
 
-async function uploadTokenToRepoSecrets(arr) {
+// repo secrets needed for sonarcloud and github authorization are created
+async function uploadTokenToRepoSecrets(arr, gitHubOrganisation, gitHubRepo) {
   for (const el of arr) {
+    
+    //Obtain the key from repo to generate a encrypted secret
     const { data: encKeyObject } = await axios.get(
-      "https://api.github.com/repos/Nikshay19/Calculator/actions/secrets/public-key",
+      `https://api.github.com/repos/${gitHubOrganisation}/${gitHubRepo}/actions/secrets/public-key`,
       {
         headers: {
-          Authorization: `token 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`,
+          Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
         },
       }
     );
@@ -24,15 +29,17 @@ async function uploadTokenToRepoSecrets(arr) {
       encKeyObject.key,
       el.value
     );
+
+    //Post the encrypted secret to github repo secrets
     await axios.put(
-      `https://api.github.com/repos/Nikshay19/Calculator/actions/secrets/${el.token_name}`,
+      `https://api.github.com/repos/${gitHubOrganisation}/${gitHubRepo}/actions/secrets/${el.token_name}`,
       {
         encrypted_value: encryptedValue,
         key_id: encKeyObject.key_id,
       },
       {
         headers: {
-          Authorization: `token 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`,
+          Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
         },
       }
     );
@@ -40,13 +47,13 @@ async function uploadTokenToRepoSecrets(arr) {
   return "upload success";
 }
 
+//Create a project in sonarcloud from github repo, by integrating the github repo to sonarcloud
 async function linkTosonarCloud(
   sonarToken,
   gitHubOrganisation,
   repoName,
   sonarOrganisation,
-  repoId,
-  branch
+  repoId
 ) {
   const projectCreateReponse = await axios({
     url: "https://sonarcloud.io/api/alm_integration/provision_projects",
@@ -61,17 +68,19 @@ async function linkTosonarCloud(
     data: `installationKeys=${gitHubOrganisation}%2F${repoName}%7C${repoId}&organization=${sonarOrganisation}`,
   });
 
+  //trigger autoscan to get metrics
   const triggerAutoScan = await axios.get(
     `https://sonarcloud.io/api/autoscan/eligibility?autoEnable=true&projectKey=${projectCreateReponse.data.projects[0].projectKey}`
   );
   console.log(triggerAutoScan.data);
-  
+
   return projectCreateReponse;
 }
 
 async function pushBuildFile(path, gitHubOrganisation, repo, commitMessage) {
   let content = fs.readFileSync("build.yml", "base64");
 
+  //push the appropriate build file to initiate project analyses by sonarcloud
   const response = await axios.put(
     `https://api.github.com/repos/${gitHubOrganisation}/${repo}/contents/${path}`,
     {
@@ -82,7 +91,7 @@ async function pushBuildFile(path, gitHubOrganisation, repo, commitMessage) {
     },
     {
       headers: {
-        Authorization: `token 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`,
+        Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
       },
     }
   );
@@ -94,24 +103,32 @@ async function createSonarCloudProjectAndLinkToGitHub(
   gitHubOrganisation,
   gitHubRepo,
   sonarOrganisation,
-  branch
+  language
 ) {
-  await uploadTokenToRepoSecrets([
-    {
-      value: sonarAuthToken,
-      token_name: "SONAR_TOKEN",
-    },
-    {
-      value: "1234rthjrthtr556",
-      token_name: "git_repo_token",
-    },
-  ]);
-  await pushBuildFile(
-    ".github/workflows/build.yml",
+  await uploadTokenToRepoSecrets(
+    [
+      {
+        value: sonarAuthToken,
+        token_name: "SONAR_TOKEN",
+      },
+      {
+        value: "1234rthjrthtr556",
+        token_name: "git_repo_token",
+      },
+    ],
     gitHubOrganisation,
-    gitHubRepo,
-    "build"
+    gitHubRepo
   );
+
+  if (language !== "javascript") {
+    await pushBuildFile(
+      ".github/workflows/build.yml",
+      gitHubOrganisation,
+      gitHubRepo,
+      "build"
+    );
+  }
+
   const { data } = await axios.get(
     `https://api.github.com/repos/${gitHubOrganisation}/${gitHubRepo}`
   );
@@ -120,35 +137,44 @@ async function createSonarCloudProjectAndLinkToGitHub(
     gitHubOrganisation,
     gitHubRepo,
     sonarOrganisation,
-    data.id,
-    branch
+    data.id
   );
-    
+
   return respData;
 }
 
-
-async function fetchMetricsFromSonarCloud(projectKey){
- const metrics = await axios.get(
+//fetch project metrics once build and analyses is complete
+async function fetchMetricsFromSonarCloud(projectKey, branch) {
+  const metrics = await axios.get(
     `https://sonarcloud.io/api/measures/component_tree?component=${projectKey}&branch=${branch}&metricKeys=complexity,violations,bugs,code_smells,lines,vulnerabilities&additionalFields=metrics`
   );
-  return metrics
+  return metrics;
 }
 
 createSonarCloudProjectAndLinkToGitHub(
   "f11d0b115fbdb7796f15a31aebe616f81654cb5e",
   "Nikshay19",
-  "Customer-Log",
+  "Calculator",
   "nikshay19",
-  "main"
+  "master",
+  "java"
 )
   .then((res) => {
-    console.log(res.data);
-    //Use only if needed  
-    fetchMetricsFromSonarCloud(res.data.projects[0].projectKey)
+    console.log(res.data ? res.data : res);
+    if (res.data && res.data.projects[0] && res.data.projects[0].projectKey) {
+      return fetchMetricsFromSonarCloud(
+        res.data.projects[0].projectKey,
+        "master"
+      );
+    }
+
+    return "unable to fetch metrics";
+  })
+  .then((res) => {
+    console.log(res.data ? res.data : res);
   })
   .catch((err) => {
-    console.log(err.response.data);
+    console.log(err.response ? err.response.data : err);
   });
 
 function encryptTokenForRepoSecret(key, value) {
